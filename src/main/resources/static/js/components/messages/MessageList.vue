b81dbc0d7952a955c123b3de21ba641b209d02ce
Marko Maend <marko.maend@gmail.com>
2020-11-16 00:30:04 +1100

    Material design with Vuetify

--- src/main/resources/static/js/components/messages/MessageList.vue
+++ src/main/resources/static/js/components/messages/MessageList.vue
@@ -1,42 +1,47 @@
 <template>
-  <div style="position: relative; width: 300px;">
-    <message-form :messages="messages" :messageAttr="message" />
-    <message-row v-for="message in messages"
-                 :key="message.id"
-                 :message="message"
-                 :editMessage="editMessage"
-                 :deleteMessage="deleteMessage"
-                 :messages="messages" />
-  </div>
+    <v-layout align-space-around justify-start column>
+        <message-form :messages="messages" :messageAttr="message" />
+        <message-row v-for="message in sortedMessages"
+                     :key="message.id"
+                     :message="message"
+                     :editMessage="editMessage"
+                     :deleteMessage="deleteMessage"
+                     :messages="messages" />1
+    </v-layout>
 </template>
 
 <script>
-import MessageRow from 'components/messages/MessageRow.vue'
-import MessageForm from 'static/js/components/messages/MessageForm'
-export default {
-  props: ['messages'],
-  components: {
-    MessageRow,
-    MessageForm
-  },
-  data() {
-    return {
-      message: null
-    }
-  },
-  methods: {
-    editMessage(message) {
-      this.message = message
-    },
-    deleteMessage(message) {
-      this.$resource('/message{/id}').remove({id: message.id}).then(result => {
-        if (result.ok) {
-          this.messages.splice(this.messages.indexOf(message), 1)
+    import MessageRow from 'components/messages/MessageRow.vue'
+    import MessageForm from 'components/messages/MessageForm.vue'
+    export default {
+        props: ['messages'],
+        components: {
+            MessageRow,
+            MessageForm
+        },
+        data() {
+            return {
+                message: null
+            }
+        },
+        computed: {
+            sortedMessages() {
+                return this.messages.sort((a, b) => -(a.id - b.id))
+            }
+        },
+        methods: {
+            editMessage(message) {
+                this.message = message
+            },
+            deleteMessage(message) {
+                this.$resource('/message{/id}').remove({id: message.id}).then(result => {
+                    if (result.ok) {
+                        this.messages.splice(this.messages.indexOf(message), 1)
+                    }
+                })
+            }
         }
-      })
     }
-  }
-}
 </script>
 
 <style>
