f41ee9ad40dfdaba2a8d2e0d4878ec44d1f5da5d
Marko Maend <marko.maend@gmail.com>
2020-11-15 14:39:17 +1100

    Spring Security and oAuth2 config in Vue.js

--- src/main/java/mmaend/Simplefb/domain/User.java
+++ src/main/java/mmaend/Simplefb/domain/User.java
@@ -1,17 +1,19 @@
 package mmaend.Simplefb.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 @Entity
 @Table(name = "usr")
 
-public class User {
+public class User implements Serializable {
     @Id
     private String id;
     private String name;
@@ -19,6 +21,7 @@ public class User {
     private String email;
     private String gender;
     private String locale;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime lastVisit;
 
     public String getId() {
