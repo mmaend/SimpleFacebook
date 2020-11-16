c79c14dd4752777291ebb3fc15e1907195fdd355
Marko Maend <marko.maend@gmail.com>
2020-11-16 20:10:11 +1100

    Add VueRouter

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -3,33 +3,36 @@
         <v-toolbar app>
             <v-toolbar-title>Simple Facebook</v-toolbar-title>
             <v-spacer></v-spacer>
-            <span v-if="profile">{{profile.name}}</span>
+            <v-btn flat
+                   v-if="profile"
+                   :disabled="$route.path === '/profile'"
+                   @click="showProfile">
+                {{profile.name}}
+            </v-btn>
             <v-btn v-if="profile" icon href="/logout">
                 <v-icon>exit_to_app</v-icon>
             </v-btn>
         </v-toolbar>
         <v-content>
-            <v-container v-if="!profile">
-              You must log in through
-                <a href="/login">Google</a>
-            </v-container>
-            <v-container v-if="profile">
-                <messages-list />
-            </v-container>
+            <router-view></router-view>
         </v-content>
     </v-app>
 </template>
 
 <script>
     import { mapState, mapMutations } from 'vuex'
-    import MessagesList from 'components/messages/MessageList.vue'
     import { addHandler } from 'util/ws'
     export default {
-        components: {
-            MessagesList
-        },
         computed: mapState(['profile']),
-        methods: mapMutations(['addMessageMutation', 'updateMessageMutation', 'removeMessageMutation']),
+        methods: {
+            ...mapMutations(['addMessageMutation', 'updateMessageMutation', 'removeMessageMutation']),
+            showMessages() {
+                this.$router.push('/')
+            },
+            showProfile() {
+                this.$router.push('/profile')
+            }
+        },
         created() {
             addHandler(data => {
                 if (data.objectType === 'MESSAGE') {
@@ -50,6 +53,11 @@
                     console.error(`Looks like the object type if unknown "${data.objectType}"`)
                 }
             })
+        },
+        beforeMount() {
+            if (!this.profile) {
+                this.$router.replace('/auth')
+            }
         }
     }
 </script>
---
+++ src/main/resources/static/js/pages/Auth.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    You must log in through
+    <a href="/login">Google</a>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Auth',
+}
+</script>
+
+<style scoped>
+</style>
