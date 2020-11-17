a7c6f8e8a3d3cbebfc71bb7d85a6327fc42d6743
Marko Maend <marko.maend@gmail.com>
2020-11-17 20:10:37 +1100

    Add Sentry

--- src/main/resources/static/js/components/messages/MessageForm
+++ src/main/resources/static/js/components/messages/MessageForm
@@ -14,12 +14,13 @@
 
 <script>
     import { mapActions } from 'vuex'
+    import * as Sentry from '@sentry/browser'
     export default {
         props: ['messageAttr'],
         data() {
             return {
                 text: '',
-                id: ''
+                id: null
             }
         },
         watch: {
@@ -41,7 +42,7 @@
                     this.addMessageAction(message)
                 }
                 this.text = ''
-                this.id = ''
+                this.id = null
             }
         }
     }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,8 +7,27 @@ import App from 'pages/App.vue'
 import store from 'store/store'
 import { connect } from './util/ws'
 import 'vuetify/dist/vuetify.min.css'
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
 
-if (frontendData.profile) {
+Sentry.init({
+    dsn: 'https://ecb6e24bd21d47e3aba16b0dfcb5679c@o477826.ingest.sentry.io/5519382',
+    integrations: [
+        new Integrations.Vue({
+            Vue,
+            attachProps: true,
+        }),
+    ],
+})
+
+Sentry.configureScope(scope =>
+    scope.setUser({
+        id: profile && profile.id,
+        username: profile && profile.name
+    })
+)
+
+if (profile) {
     connect()
 }
 
